27a96372f20e28fa644f774a5c0925b015c5ea8b
kanuff <makerman4@gmail.com>
2019-06-13 23:46:59 -0400

    Define methods for Stack class and add basic test cases

--- W1D4/stack.rb
+++ W1D4/stack.rb
@@ -1,17 +1,29 @@
   class Stack
-    def initialize
+    def initialize(stack=[])
       # create ivar to store stack here!
+      @stack = stack
     end
 
     def push(el)
       # adds an element to the stack
+      @stack << el
     end
 
     def pop
       # removes one element from the stack
+      @stack.pop
     end
 
     def peek
       # returns, but doesn't remove, the top element in the stack
+      @stack.last
     end
   end
+
+if __FILE__ == $0
+    p stack = Stack.new{[]}
+    p stack.push(4) # ==> [4]
+    p stack.push(6) # ==> [4, 6]
+    p stack.pop     # ==> 6
+    p stack.peek    # ==> 4
+end
