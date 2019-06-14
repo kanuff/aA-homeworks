8538de487947b828456eb40d8afe9164dc025bf7
kanuff <makerman4@gmail.com>
2019-06-13 23:55:29 -0400

    Create skeleton for Map class

---
+++ W1D4/map.rb
@@ -0,0 +1,23 @@
+class Map
+
+    def initialize(map=[])
+        @map = map
+    end
+
+    def set(key, value)
+
+    end
+
+
+
+    def get(key)
+    end
+
+    def delete(key)
+    end
+
+    def show
+        @map
+    end
+
+end
