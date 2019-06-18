4761687318e3134018f83503079e8053daa81745
kanuff <makerman4@gmail.com>
2019-06-18 06:04:54 -0400

    Implement working Simon game

--- W2D2/simon/lib/simon.rb
+++ W2D2/simon/lib/simon.rb
@@ -4,38 +4,70 @@ class Simon
   attr_accessor :sequence_length, :game_over, :seq
 
   def initialize
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 
   def play
-
+    take_turn
+    until @game_over
+      take_turn
+    end
+    game_over_message
+    reset_game
   end
 
   def take_turn
-
+    show_sequence
+    require_sequence
+    unless game_over
+      round_success_message
+      add_random_color
+      self.sequence_length += 1
+    end
   end
 
   def show_sequence
-
+    add_random_color
+    @seq.each do |color|
+      puts color
+    end
+    sleep(1 + @seq.length/2)
   end
 
   def require_sequence
-
+    system("clear")
+    puts "Type the color sequence pressing enter each time"
+    @seq.each do |color|
+      input = gets.chomp
+      if input != color
+        @game_over = true
+        break
+      end
+    end
   end
 
   def add_random_color
-
+    
+    @seq << COLORS.sample
   end
 
   def round_success_message
-
+    puts "Good job"
   end
 
   def game_over_message
-
+    puts "You're a failure"
   end
 
   def reset_game
-
+    @sequence_length = 1
+    @game_over = false
+    @seq = []
   end
 end
+
+
+# simon = Simon.new
+# simon.play
