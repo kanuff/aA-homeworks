b8dbfe5f17a812016ed7fa038a7f62a322bc46ac
kanuff <makerman4@gmail.com>
2019-06-24 22:57:47 -0400

    Update ORM with Playwright functionality

--- W3D2/skeleton/plays.rb
+++ W3D2/skeleton/plays.rb
@@ -19,6 +19,16 @@ class Play
     data.map { |datum| Play.new(datum) }
   end
 
+  def self.find_by_title(title)
+    data = PlayDBConnection.instance.execute("SELECT * FROM plays JOIN playwrights ON plays.playwright_id = playwright.id WHERE name = '#{name}'")
+    data.map { |datum| Play.new(datum) }
+  end
+
+  def self.find_by_playwright(name)
+    data = PlayDBConnection.instance.execute("SELECT * FROM plays WHERE playwright = '#{title}'")
+    data.map { |datum| Play.new(datum) }
+  end
+
   def initialize(options)
     @id = options['id']
     @title = options['title']
@@ -48,4 +58,56 @@ class Play
         id = ?
     SQL
   end
+end
+
+
+class Playwright
+
+  attr_accessor :id, :name, :birth_year
+
+  
+  def self.all
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights")
+    data.map { |datum| Play.new(datum) }
+  end
+
+  def self.find_by_name(name)
+    data = PlayDBConnection.instance.execute("SELECT * FROM playwrights WHERE name = '#{name}'")
+    data.map { |datum| Play.new(datum) }
+  end
+
+
+  def initialize(options)
+    @id = options['id']
+    @name = options['name']
+    @birth_year = options['birth_year']
+  end
+  def create
+    raise "#{self} already in database" if self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year)
+      INSERT INTO
+        playwrights (name, birth_year)
+      VALUES
+        (?, ?)
+    SQL
+    self.id = PlayDBConnection.instance.last_insert_row_id
+  end
+
+  def update
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute(<<-SQL, self.name, self.birth_year, self.id)
+      UPDATE
+        playwrights
+      SET
+        name = ?, birth_year = ?
+      WHERE
+        id = ?
+    SQL
+  end
+
+  def get_plays
+    raise "#{self} not in database" unless self.id
+    PlayDBConnection.instance.execute("SELECT * FROM playwrights WHERE name = '#{@name}' '")
+  end
+
 end
